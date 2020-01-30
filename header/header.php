17b28d19453da661fe670fcc0047b46a6d6652fc
AlbertoMoya <38519723+AlbertoMoya@users.noreply.github.com>
2020-01-29 21:13:40 -0600

    Add files via upload

---
+++ header/header.php
@@ -0,0 +1,64 @@
+<head>
+	<meta charset="UTF-8">
+	<title>JGA Asesores de Negocios</title>
+	<link rel="stylesheet" href="estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Alberto Moya">
+    
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,600" rel="stylesheet">
+
+    <!-- Ionic icons-->
+    <link href="https://unpkg.com/ionicons@4.5.5/dist/css/ionicons.min.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/L1.png"/>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <img src="img/logo_jga.png" class="logo-brand" alt="logo">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="icon ion-md-menu"></i>
+            </button>
+        
+         <!-- BARRA MENU -->
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                        <a class="nav-link" href="../contenido/contenido.php">INICIO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../perfil/perfil2.php">PERFIL</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../vacantes/index.php">VACANTES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../postulados/index.php">POSTULADOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../candidatos/select.php">CANDIDATOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../log2/logout.php">CERRAR SESIÓN</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
